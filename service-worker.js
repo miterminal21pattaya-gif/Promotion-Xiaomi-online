78e2e5f4bd8fee12a7f0b746e32594ebdd9f4b84
miterminal21pattaya-gif <miterminal21pattaya@gmail.com>
2026-08-17 14:10:06 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,57 @@
 const CACHE_NAME = "xiaomi-admin-v1";
 
-const FILES = [
+const FILES_TO_CACHE = [
   "./admin-app.html",
-  "./admin.html",
   "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+
+    caches.open(CACHE_NAME).then(cache => {
+
+      return cache.addAll(FILES_TO_CACHE);
+
+    })
+
   );
+
   self.skipWaiting();
+
 });
+
 
 self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+
+  event.waitUntil(
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+
+      );
+
+    })
+
+  );
+
+  self.clients.claim();
+
 });
 
+
 self.addEventListener("fetch", event => {
+
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
+
   );
+
 });
