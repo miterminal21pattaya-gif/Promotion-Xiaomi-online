b7c0e09d8a60cb23896eb575634fcd1a51295d1a
miterminal21pattaya-gif <miterminal21pattaya@gmail.com>
2026-08-17 14:20:48 +0700

    admin-sw.js

--- admin-sw.js
+++ admin-sw.js
@@ -1,19 +1,17 @@
 const CACHE_NAME = "xiaomi-admin-v1";
 
-const FILES_TO_CACHE = [
-  "./admin-app.html",
-  "./manifest.json"
+const FILES = [
+  "/admin-app.html",
+  "/admin-manifest.json",
+  "/admin.html"
 ];
 
 self.addEventListener("install", event => {
 
   event.waitUntil(
 
-    caches.open(CACHE_NAME).then(cache => {
-
-      return cache.addAll(FILES_TO_CACHE);
-
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
 
   );
 
@@ -21,29 +19,27 @@ self.addEventListener("install", event => {
 
 });
 
-
 self.addEventListener("activate", event => {
 
   event.waitUntil(
 
-    caches.keys().then(keys => {
+    caches.keys().then(keys =>
 
-      return Promise.all(
+      Promise.all(
 
         keys
           .filter(key => key !== CACHE_NAME)
           .map(key => caches.delete(key))
 
-      );
+      )
 
-    })
+    )
 
   );
 
   self.clients.claim();
 
 });
-
 
 self.addEventListener("fetch", event => {
 
